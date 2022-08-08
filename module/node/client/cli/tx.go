325a0639b109f7abe0b3d0aea105880b945b3cc2
Dreamer <745124335@qq.com>
2022-08-08 11:03:44 +0800

    improve command comment

--- module/node/client/cli/tx.go
+++ module/node/client/cli/tx.go
@@ -1,11 +1,15 @@
 package cli
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/spf13/cobra"
 
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/client/tx"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/version"
 	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
 
 	"github.com/bianjieai/spartan-cosmos/module/node/client/utils"
@@ -18,6 +22,30 @@ func NewCreateValidatorProposalTxCmd() *cobra.Command {
 		Use:   "create-validator [file]",
 		Args:  cobra.ExactArgs(1),
 		Short: "Submit a validator create proposal",
+		Long: strings.TrimSpace(
+			fmt.Sprintf(`Submit a proposal along with an initial deposit.
+Example:
+$ %s tx gov submit-proposal create-validator proposal.json --from mykey
+
+Where proposal.json contains:
+
+{
+	"deposit":"1000usnmt",
+	"content":{
+		"title":"proposal title",
+		"summary":"proposal description",
+		"name":"node1",
+		"certificate":"-----BEGIN CERTIFICATE-----\nMIIBbDCCAR4CFGX5t8OFPrR8Ul/1JoXj3CFBBrBvMAUGAytlcDBYMQswCQYDVQQG\nEwJDTjENMAsGA1UECAwEcm9vdDENMAsGA1UEBwwEcm9vdDENMAsGA1UECgwEcm9v\ndDENMAsGA1UECwwEcm9vdDENMAsGA1UEAwwEcm9vdDAeFw0yMjA4MDUxMTA0Mzda\nFw0yMzA4MDUxMTA0MzdaMFkxCzAJBgNVBAYTAkNOMQ0wCwYDVQQIDAR0ZXN0MQ0w\nCwYDVQQHDAR0ZXN0MQ0wCwYDVQQKDAR0ZXN0MQ0wCwYDVQQLDAR0ZXN0MQ4wDAYD\nVQQDDAVub2RlMDAqMAUGAytlcAMhACJAXK+iRqiKcapdgM3aKrP39Mwp/gGjUDPS\nWo3+qM8vMAUGAytlcANBAP0JsV3l8PW65sxgxx3dBAQbagCDimvcxVtwIIF/rWPo\ndLTSmKoX6XNtyq5O5bU8Xj8mypeWqQd0SQe65MQqvQc=\n-----END CERTIFICATE-----\n",
+	    "power":"100",
+	    "description":"my node1",
+	    "operator":"iaa104hrdtdkk5lfh8c3nc3pf20ad0sgdvselg0vxs"
+	}
+}
+
+`,
+				version.AppName,
+			),
+		),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			clientCtx, err := client.GetClientTxContext(cmd)
 			if err != nil {
@@ -52,6 +80,31 @@ func NewUpdateValidatorProposalTxCmd() *cobra.Command {
 		Use:   "update-validator [file]",
 		Args:  cobra.ExactArgs(1),
 		Short: "Submit a validator update proposal",
+		Long: strings.TrimSpace(
+			fmt.Sprintf(`Submit a proposal along with an initial deposit.
+Example:
+$ %s tx gov submit-proposal update-validator proposal.json --from mykey
+
+Where proposal.json contains:
+
+{
+	"deposit":"1000usnmt",
+	"content":{
+		"title":"proposal title",
+		"summary":"proposal description",
+		"id":"xxxxxx",
+		"name":"node1",
+		"certificate":"-----BEGIN CERTIFICATE-----\nMIIBbDCCAR4CFGX5t8OFPrR8Ul/1JoXj3CFBBrBvMAUGAytlcDBYMQswCQYDVQQG\nEwJDTjENMAsGA1UECAwEcm9vdDENMAsGA1UEBwwEcm9vdDENMAsGA1UECgwEcm9v\ndDENMAsGA1UECwwEcm9vdDENMAsGA1UEAwwEcm9vdDAeFw0yMjA4MDUxMTA0Mzda\nFw0yMzA4MDUxMTA0MzdaMFkxCzAJBgNVBAYTAkNOMQ0wCwYDVQQIDAR0ZXN0MQ0w\nCwYDVQQHDAR0ZXN0MQ0wCwYDVQQKDAR0ZXN0MQ0wCwYDVQQLDAR0ZXN0MQ4wDAYD\nVQQDDAVub2RlMDAqMAUGAytlcAMhACJAXK+iRqiKcapdgM3aKrP39Mwp/gGjUDPS\nWo3+qM8vMAUGAytlcANBAP0JsV3l8PW65sxgxx3dBAQbagCDimvcxVtwIIF/rWPo\ndLTSmKoX6XNtyq5O5bU8Xj8mypeWqQd0SQe65MQqvQc=\n-----END CERTIFICATE-----\n",
+	    "power":"100",
+	    "description":"my node1",
+	    "operator":"iaa104hrdtdkk5lfh8c3nc3pf20ad0sgdvselg0vxs"
+	}
+}
+
+`,
+				version.AppName,
+			),
+		),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			clientCtx, err := client.GetClientTxContext(cmd)
 			if err != nil {
@@ -86,6 +139,27 @@ func NewRemoveValidatorProposalTxCmd() *cobra.Command {
 		Use:   "remove-validator [file]",
 		Args:  cobra.ExactArgs(1),
 		Short: "Submit a validator remove proposal",
+		Long: strings.TrimSpace(
+			fmt.Sprintf(`Submit a proposal along with an initial deposit.
+Example:
+$ %s tx gov submit-proposal remove-validator proposal.json --from mykey
+
+Where proposal.json contains:
+
+{
+	"deposit":"1000usnmt",
+	"content":{
+		"title":"proposal title",
+		"summary":"proposal description",
+		"id":"xxxxxx",
+	    "operator":"iaa104hrdtdkk5lfh8c3nc3pf20ad0sgdvselg0vxs"
+	}
+}
+
+`,
+				version.AppName,
+			),
+		),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			clientCtx, err := client.GetClientTxContext(cmd)
 			if err != nil {
